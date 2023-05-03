e0b25ac44359f8b4b7525e9aa3570d93ceb16d75
Daniel Bou de Souza <daniel.bou@nava.com.br>
2023-05-03 11:16:12 -0300

    dotenv

--- api-rest-mongodb/src/config/dbConnect.js
+++ api-rest-mongodb/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/alura-node?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2");
+mongoose.connect(process.env.DB_CONNECTION);
 
 let db = mongoose.connection;
 
