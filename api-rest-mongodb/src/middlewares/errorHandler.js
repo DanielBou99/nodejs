746c78fcae52b75e36e9abcf91f52314e281a5c4
Daniel Bou de Souza <daniel.bou@nava.com.br>
2023-05-07 11:15:10 -0300

    Tatar erro validação entidades

--- api-rest-mongodb/src/middlewares/errorHandler.js
+++ api-rest-mongodb/src/middlewares/errorHandler.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 
 // eslint-disable-next-line no-unused-vars
 function errorHandler(error, req, res, next) {
+  console.log(error);
   if (error instanceof mongoose.Error.CastError) {
     res.status(400).send({message: "Incorrect data requested"});
+  } else if(error instanceof mongoose.Error.ValidationError) {
+    const errorsMessages = Object.values(error.errors)
+      .map(error => error.message)
+      .join("; ");
+    res.status(400).send({message: `Error: ${errorsMessages}`});
   } else {
     res.status(500).send({message: `${error.message} - Server Error`});
   }    
--- api-rest-mongodb/src/models/Book.js
+++ api-rest-mongodb/src/models/Book.js
@@ -2,9 +2,18 @@ import mongoose from "mongoose";
 
 const bookSchema = new mongoose.Schema({
   id: { type: String },
-  title: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "authors", required: true },
-  publisher: { type: String, required: true },
+  title: { 
+    type: String, 
+    required: [true, "Book title is required"] 
+  },
+  author: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "authors", required: [true, "Author is required"] 
+  },
+  publisher: { 
+    type: String, 
+    required: [true, "Publisher is required"] 
+  },
   pages: { type: Number },
 });
 
